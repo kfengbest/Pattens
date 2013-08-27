d2f63692139b3e7c0c12dd48265004ca6c945240
kfengbest <kfengbest@hotmail.com>
2013-08-27 19:05:33 +0800

    Added Task

---
+++ TestPattens/TestPattens/Tasks.h
@@ -0,0 +1,14 @@
+//
+//  Tasks.h
+//  TestPattens
+//
+//  Created by Kaven Feng on 8/25/13.
+//  Copyright (c) 2013 Kaven Feng. All rights reserved.
+//
+
+#ifndef __TestPattens__Tasks__
+#define __TestPattens__Tasks__
+
+#include <iostream>
+
+#endif /* defined(__TestPattens__Tasks__) */
